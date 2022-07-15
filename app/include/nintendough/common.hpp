7442a62256879f919c48e9a412b77eb9fe6b2a3a
Matt Williams <mattltf@protonmail.com>
2022-07-15 11:21:45 -0700

    :octocat: Common standard library barrel file

---
+++ app/include/nintendough/common.hpp
@@ -0,0 +1,32 @@
+#ifndef NINTENDOUGH_COMMON_HPP
+#define NINTENDOUGH_COMMON_HPP
+
+/**
+ * @file nintendough/common.hpp
+ * @author Matt Williams (matt.k.williams@protonmail.com)
+ * @brief Barrel file for common standard library includes
+ * @date 2021-11-19
+ */
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <chrono>
+#include <condition_variable>
+#include <cstdint>
+#include <cstdlib>
+#include <deque>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <sstream>
+#include <string_view>
+#include <thread>
+#include <variant>
+#include <vector>
+
+#endif
